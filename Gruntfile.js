78783bd0250e7df6efd92f292abb9f844641e775
Shivalik <shvlksen@gmail.com>
2015-11-22 02:43:10 +0530

    moar debugging

--- Gruntfile.js
+++ Gruntfile.js
@@ -479,7 +479,7 @@ module.exports = function (grunt) {
   });
 
   //trying to get grunt to build on heroku
-  grunt.registerTask('heroku', ['default']);
+  grunt.registerTask('heroku', ['build']);
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
