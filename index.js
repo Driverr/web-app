096d2518740d81c913195eecbc22ebcca96516ae
Shivalik <shvlksen@gmail.com>
2015-11-21 08:05:00 +0530

    more and more debug

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var http = require('http');
 var gzippo = require('gzippo');
 
 var app = express();
-app.use(gzippo.staticGzip('' + __dirname + '/dist'));
+app.use(gzippo.staticGzip('views' + __dirname + '/dist'));
 
-/*app.use('/*', function(req, res) {
+app.use('/*', function(req, res) {
 	res.sendfile( __dirname + '/index.html');
-});*/
+});
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
