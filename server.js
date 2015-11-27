955084d9202c96ba0abfd219e79821c93e924405
devikalal333 <devikalal333@gmail.com>
2015-11-27 19:36:34 +0530

    server changes for backend printing

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-/*app.use('/assets', express.static(__dirname + "/app/assets"));
+app.use('/assets', express.static(__dirname + "/app/assets"));
+app.use('/bower_components', express.static(__dirname + "/bower_components"));
 app.use('/css', express.static(__dirname + "/app/assets/css"));
 app.use('/js', express.static(__dirname + "/app/assets/js"));
 app.use('/img', express.static(__dirname + "/app/assets/img"));
 app.use('/fonts', express.static(__dirname + "/app/assets/fonts"));
 app.use('/views', express.static(__dirname + "/app/assets/views"))
-*/
-app.listen(process.env.PORT || 5000);
+
+app.listen(process.env.PORT || 5001);
