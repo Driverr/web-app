d2ceb0968f75fde5cd694f99783438766879a919
Shivalik <shvlksen@gmail.com>
2015-12-02 17:35:21 +0530

    used gzippo on all folders, removed font awesome css to try

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ var express = require('express');
 var app = express();
 
 app.use(morgan('dev'));
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+
+app.use(gzippo.staticGzip(__dirname + "/app"));
+app.use('/assets', gzippo.staticGzip(__dirname + "/app/assets"));
+app.use('/bower_components', gzippo.staticGzip(__dirname + "/bower_components"));
+app.use('/css', gzippo.staticGzip(__dirname + "/app/assets/css"));
+app.use('/js', gzippo.staticGzip(__dirname + "/app/assets/js"));
+app.use('/img', gzippo.staticGzip(__dirname + "/app/assets/img"));
+app.use('/fonts', gzippo.staticGzip(__dirname + "/app/assets/fonts"));
+app.use('/views', gzippo.staticGzip(__dirname + "/app/assets/views"))
+
+
+
+/*
 app.use(express.static(__dirname + "/app"));
 app.use('/assets', express.static(__dirname + "/app/assets"));
 app.use('/bower_components', express.static(__dirname + "/bower_components"));
@@ -13,6 +25,6 @@ app.use('/css', express.static(__dirname + "/app/assets/css"));
 app.use('/js', express.static(__dirname + "/app/assets/js"));
 app.use('/img', express.static(__dirname + "/app/assets/img"));
 app.use('/fonts', express.static(__dirname + "/app/assets/fonts"));
-app.use('/views', express.static(__dirname + "/app/assets/views"))
+app.use('/views', express.static(__dirname + "/app/assets/views"))*/
 
 app.listen(process.env.PORT || 5001);
